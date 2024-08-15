a9efb3c8a39297f31c74272a92cb8fb079fae48c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-16 00:19:00 +0900

    웹팩 설정 파일 셋팅

---
+++ dev-server/webpack.config.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [],
+};
